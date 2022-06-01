359cdca5e8b90e90c5f62973782d7cfcfd025b4d
Gr0tten0lm <106630091+Gr0tten0lm@users.noreply.github.com>
2022-06-01 17:10:39 +0000

    Update install.py

--- install.py
+++ install.py
@@ -5,4 +5,4 @@ os.system('pip install qrcode')
 os.system('pip install requests')
 os.system('pip install colorama')
 os.system('pip install paramiko')
-os.system('sudo apt-get install python-opencv'
+os.system('sudo apt-get install python-opencv')
